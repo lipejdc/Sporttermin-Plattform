85fa525921f6fb62d8a0726ad817534046d2a3d9
AndreasNiesen <andreas.niesen91@web.de>
2024-02-26 10:53:09 +0100

    API service swaparound

--- Backend/API.py
+++ Backend/API.py
@@ -1,98 +1,211 @@
+import service
 import json
-import SQL_Alchemy_Stuff as ORM
-from SQL_Alchemy_Stuff import User, Appointment, Comment, Friendzone
-from sqlalchemy import select
+import hashlib
+import datetime
+import re
+import os
+from flask import Flask, request, jsonify, session, render_template, redirect
 
-def user_get(id: str = "", email: str = "", passwd:str = "") -> set:
-    with ORM.get_session() as session:
-        if id != "":
-            user = session.scalars(select(User).where(User.id == id)).one()
-            
-            if user:
-                return user.to_json()
-            
-        elif email != "" and passwd != "":
-            user = session.scalars(select(User).where(User.id == id)).one()
-            
-            if user:
-                return user.to_json()
-        else:
-            users = session.scalars(select(User)).all()
+template_dir = os.path.abspath("../Frondend")
+app = Flask(__name__, template_folder=template_dir)
+app.secret_key = "OSP_SECRET_KEY::TODO:CHANGE!"
+
+active_users = {}  # key: session_id, value: user_id
+
+
+def get_user_id_from_session(session: dict) -> str:
+    try:
+        session_id = session["session_id"]
+        return active_users[session_id]
+    except Exception as e:
+        print(f"Exception in get_user_id_from_session: {e}")
+        return ""
+
+
+def hash_pw(unhashed_pw: str) -> str:
+    return hashlib.sha256(unhashed_pw.encode("utf-8")).hexdigest()
+
+
+@app.route("/", methods=["GET"])
+def login_page():
+    return render_template('loginPage/loginPage.html')
+
+
+@app.route("/", methods=["POST"])
+def login():
+    content = request.json
+    
+    email = content["email"]
+    passwd = content["passwd"]
+    
+    user_list = json.loads(service.user_get(email=email, passwd=passwd))
+    if len(user_list.keys()) == 1:
+        unencoded_session_id = user_list.keys()[0] + str(datetime.datetime.now())
+        session_id = hashlib.sha256(unencoded_session_id.encode("utf-8")).hexdigest()
+        session["session_id"] = session_id
+        active_users["session_id"] = user_list["id"]
         
-            users_dict = {}
-            for user in users:
-                users_dict[user.id] = user.to_dict()
-                
-            return json.dumps(users_dict)
-            
-        return "{}"
+        return {}, 200
+    
+    return {}, 404  # error_msg="Nutzername oder Passwort falsch"
 
 
-def user_update(id: str, first_name: str, last_name: str, email: str, passwd: str, gender: int, age: int, city: str):
-    with ORM.get_session() as session:
-        user = session.scalars(select(User).where(User.id == id)).one()
+# Registration will be a Pop-Up on the Login-Page,
+# therefore a GET to render a register-page will not be necessary
+@app.route("/register", methods=["POST"])
+def register():
+    content = request.json
+    
+    first_name = content["first_name"]
+    last_name = content["last_name"]
+    email = content["email"]
+    passwd = content["passwd"]
+    gender = content["gender"]
+    age = content["age"]
+    city = content["city"]
+    
+    errors = 0
+    msg = ""
+    
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        errors += 1
+        msg += "Die E-Mail-Adresse ist nicht valide\r\n"
         
-        if user:
-            user.first_name = first_name
-            user.last_name = last_name
-            user.email = email
-            user.passwd = passwd
-            user.gender = gender
-            user.age = age
-            user.city = city
-            
-            session.commit()
-            
-            return user.to_json()
-            
-    return ("{}")
-
-
-def user_create(first_name: str, last_name: str, email: str, passwd: str, gender: int, age: int, city: str):
-    with ORM.get_session() as session:
-        user = User()
+    try:
+        gender = int(gender)
+    except:
+        errors += 1
+        msg += "Gender hat keinen validen Wert\r\n"
         
-        if user:
-            user.first_name = first_name
-            user.last_name = last_name
-            user.email = email
-            user.passwd = passwd
-            user.gender = gender
-            user.age = age
-            user.city = city
-            
-            session.add(user)
-            session.commit()
-            
-            return user.to_json()
-            
-    return ("{}")
+    try:
+        age = int(age)
+    except:
+        errors += 1
+        msg += "Alter hat keinen validen Wert\r\n"
+        
+    if errors > 0:
+        print("Error registering User: {msg}")
+        return {}, 404
+    
+    # No clear pass on the db!
+    passwd = hash_pw(passwd)
+    
+    user_list = json.loads(service.user_create(first_name, last_name, email, passwd, gender, age, city))
+    if len(user_list.keys()) == 1:
+        unencoded_session_id = user_list.keys()[0] + str(datetime.datetime.now())
+        session_id = hashlib.sha256(unencoded_session_id.encode("utf-8")).hexdigest()
+        session["session_id"] = session_id
+        active_users["session_id"] = user_list["id"]
+        
+        return {}, 200
+    
+    return {}, 404  # error_msg="Etwas ist schief gegangen, bitte Informieren sie den Support."
 
 
-def user_delete(id: str):
-    with Session(engine) as session:
+@app.route("/home/", methods=["GET"])
+def home_page():
+    user_id = get_user_id_from_session(session)
+    
+    if user_id == "":
+        return {}, 401
+    
+    # send all upcoming appointments to the site, as this is the standard-view/-tab
+    appointments = service.apt_get(user_id=user_id)
+    if len(appointments) > 1:
+        appointments = sorted(appointments, reverse=True, key= lambda apt: apt["date"])
+    
+    return {"appointments": appointments}, 200
+
+
+# Settings will be a Pop-Up/Slide-In on the homepage
+# therefore a GET to render a settings-page will not be necessary
+@app.route("/api/user/", methods=["PUT"])
+def change_profile():
+    user_id = get_user_id_from_session(session)
+    
+    if user_id == "":
+        return jsonify({"error_msg": "Bitte einloggen!"})
+    
+    user_list = json.loads(service.user_get(id=user_id))
+    if len(user_list.keys()) != 1:
+        return jsonify({"error_msg": "Bitte registrieren!"})
+    
+    user_info = user_list[0]
+    
+    content = request.json
+    
+    first_name = content["first_name"]
+    last_name = content["last_name"]
+    email = content["email"]
+    passwd = content["passwd"]
+    gender = content["gender"]
+    age = content["age"]
+    city = content["city"]
+    
+    errors = 0
+    msg = ""
+    
+    if user_info["passwd"] == hash_pw(passwd):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            errors += 1
+            msg += "Die E-Mail-Adresse ist nicht valide\r\n"
+            
         try:
-            user = session.scalars(select(User).where(User.id == id)).one()
+            gender = int(gender)
+        except:
+            errors += 1
+            msg += "Gender hat keinen validen Wert\r\n"
             
-            if(user):
-                session.delete(user)
-                session.commit()
-                return Response("OK", status=200)
-            else:
-                return Response("invalid request", status=400)
-            
-        except Exception as e:
-            print(f"Exception in delete_user: {e}")
-            return Response("invalid request", status=400)
+        try:
+            age = int(age)
+        except:
+            errors += 1
+            msg += "Alter hat keinen validen Wert\r\n"
+    else:
+        errors += 1
+        msg = "Passwort Falsch"
+        
+    if errors > 0:
+        return {}, 404
+    
+    # changing email or password is currently not allowed
+    service.user_update(user_id, first_name, last_name, user_info["email"], user_info["passwd"], gender, age, city)
+    
+    return {}, 200
 
 
-def fz_get(user_id: str, fz_id: str = "") -> str:
-    print("hi000er!")
+@app.route("/api/Friendzone/", methods=["GET"])
+@app.route("/api/Friendzone/<fz_id: str>", methods=["GET"])
+def fz_get(fz_id=""):
+    user_id = get_user_id_from_session(session)
+    
+    if user_id == "":
+        return {}, 401
+    
+    return service.fz_get(user_id, fz_id), 200
 
 
-def apt_get(user_id: str, apt_id: str = "") -> str:
-    print("hi000er!")
+@app.route("/api/Appointment/", methods=["GET"])
+@app.route("/api/Appointment/<apt_id: str>", methods=["GET"])
+def apt_get(apt_id=""):
+    user_id = get_user_id_from_session(session)
+    
+    if user_id == "":
+        return {}, 401
+    
+    return service.apt_get(user_id, apt_id), 200
 
 
-def comment_get(user_id: str, apt_id: str = "") -> str:
-    print("hi000er!")
+@app.route("/api/Comment/", methods=["GET"])
+@app.route("/api/Comment/<apt_id: str>", methods=["GET"])
+def comment_get(apt_id=""):
+    user_id = get_user_id_from_session(session)
+    
+    if user_id == "":
+        return {}, 401
+    
+    return service.comment_get(user_id, apt_id), 200
+
+
+if __name__ == "__main__":
+    app.run()
--- Backend/service.py
+++ Backend/service.py
@@ -1,199 +1,329 @@
-import API
 import json
-import hashlib
-import datetime
-import re
-from flask import Flask, request, jsonify, session, render_template, redirect
+import dbclasses as ORM
+from dbclasses import User, Appointment, Comment, Friendzone
+from sqlalchemy import select
+from datetime import datetime
 
+ORM.create_db_if_not_exists()
 
-app = Flask(__name__)
-app.secret_key = "OSP_SECRET_KEY::TODO:CHANGE!"
-
-active_users = {}  # key: session_id, value: user_id
-
-
-def get_user_id_from_session(session: dict) -> str:
-    try:
-        session_id = session["session_id"]
-        return active_users[session_id]
-    except Exception as e:
-        print(f"Exception in get_user_id_from_session: {e}")
-        return ""
-
-
-def hash_pw(unhashed_pw: str) -> str:
-    return hashlib.sha256(unhashed_pw.encode("utf-8")).hexdigest()
-
-
-@app.route("/", methods=["GET"])
-def login_page():
-    return render_template('login.html')
-
-
-@app.route("/", methods=["POST"])
-def login():
-    content = request.json
-    
-    email = content["email"]
-    passwd = content["passwd"]
-    
-    user_list = json.loads(API.user_get(email=email, passwd=passwd))
-    if len(user_list.keys()) == 1:
-        unencoded_session_id = user_list.keys()[0] + str(datetime.datetime.now())
-        session_id = hashlib.sha256(unencoded_session_id.encode("utf-8")).hexdigest()
-        session["session_id"] = session_id
-        active_users["session_id"] = user_list["id"]
-        
-        return redirect("/home/", code=302)
-    
-    return render_template('login.html', error_msg="Nutzername oder Passwort falsch")
-
-
-# Registration will be a Pop-Up on the Login-Page,
-# therefore a GET to render a register-page will not be necessary
-@app.route("/register", methods=["POST"])
-def register():
-    content = request.json
-    
-    first_name = content["first_name"]
-    last_name = content["last_name"]
-    email = content["email"]
-    passwd = content["passwd"]
-    gender = content["gender"]
-    age = content["age"]
-    city = content["city"]
-    
-    errors = 0
-    msg = ""
-    
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        errors += 1
-        msg += "Die E-Mail-Adresse ist nicht valide\r\n"
-        
-    try:
-        gender = int(gender)
-    except:
-        errors += 1
-        msg += "Gender hat keinen validen Wert\r\n"
-        
-    try:
-        age = int(age)
-    except:
-        errors += 1
-        msg += "Alter hat keinen validen Wert\r\n"
-        
-    if errors > 0:
-        return render_template('login.html', error_msg=msg)
-    
-    # No clear pass on the db!
-    passwd = hash_pw(passwd)
-    
-    user_list = json.loads(API.user_create(first_name, last_name, email, passwd, gender, age, city))
-    if len(user_list.keys()) == 1:
-        unencoded_session_id = user_list.keys()[0] + str(datetime.datetime.now())
-        session_id = hashlib.sha256(unencoded_session_id.encode("utf-8")).hexdigest()
-        session["session_id"] = session_id
-        active_users["session_id"] = user_list["id"]
-        
-        return redirect("/home/", code=302)
-    
-    return render_template('login.html', error_msg="Etwas ist schief gegangen, bitte Informieren sie den Support.")
-
-
-@app.route("/home/", methods=["GET"])
-def home_page():
-    return render_template('home.html')
-
-
-# Settings will be a Pop-Up/Slide-In on the homepage
-# therefore a GET to render a settings-page will not be necessary
-@app.route("/api/user/", methods=["PUT"])
-def change_profile():
-    user_id = get_user_id_from_session(session)
-    
-    if user_id == "":
-        return jsonify({"error_msg": "Bitte einloggen!"})
-    
-    user_list = json.loads(API.user_get(id=user_id))
-    if len(user_list.keys()) != 1:
-        return jsonify({"error_msg": "Bitte registrieren!"})
-    
-    user_info = user_list[0]
-    
-    content = request.json
-    
-    first_name = content["first_name"]
-    last_name = content["last_name"]
-    email = content["email"]
-    passwd = content["passwd"]
-    gender = content["gender"]
-    age = content["age"]
-    city = content["city"]
-    
-    errors = 0
-    msg = ""
-    
-    if user_info["passwd"] == hash_pw(passwd):
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            errors += 1
-            msg += "Die E-Mail-Adresse ist nicht valide\r\n"
+def user_get(id: str = "", email: str = "", passwd:str = "") -> set:
+    with ORM.get_session() as session:
+        if id != "":
+            user = session.scalars(select(User).where(User.id == id)).one()
             
-        try:
-            gender = int(gender)
-        except:
-            errors += 1
-            msg += "Gender hat keinen validen Wert\r\n"
+            if user:
+                return user.to_json()
             
-        try:
-            age = int(age)
-        except:
-            errors += 1
-            msg += "Alter hat keinen validen Wert\r\n"
-    else:
-        errors += 1
-        msg = "Passwort Falsch"
+        elif email != "" and passwd != "":
+            user = session.scalars(select(User).where(User.id == id)).one()
+            
+            if user:
+                return user.to_json()
+        else:
+            users = session.scalars(select(User)).all()
         
-    if errors > 0:
-        return jsonify({"error_msg": msg.strip()})
-    
-    # changing email or password is currently not allowed
-    API.user_update(user_id, first_name, last_name, user_info["email"], user_info["passwd"], gender, age, city)
-    
-    return jsonify({"error_msg": ""})
+            users_dict = {}
+            for user in users:
+                users_dict[user.id] = user.to_dict()
+                
+            return json.dumps(users_dict)
+            
+        return ("{}")
 
 
-@app.route("/api/Friendzone/", methods=["GET"])
-@app.route("/api/Friendzone/<fz_id: str>", methods=["GET"])
-def fz_get(fz_id=""):
-    user_id = get_user_id_from_session(session)
-    
-    if user_id == "":
-        return jsonify({"error_msg": "Bitte einloggen!"})
-    
-    return API.fz_get(user_id, fz_id)
+def user_update(id: str, first_name: str, last_name: str, email: str, passwd: str, gender: int, age: int, city: str):
+    with ORM.get_session() as session:
+        user = session.scalars(select(User).where(User.id == id)).one()
+        
+        if user:
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.passwd = passwd
+            user.gender = gender
+            user.age = age
+            user.city = city
+            
+            session.commit()
+            
+            return user.to_json()
+            
+    return ("{}")
 
 
-@app.route("/api/Appointment/", methods=["GET"])
-@app.route("/api/Appointment/<apt_id: str>", methods=["GET"])
-def apt_get(apt_id=""):
-    user_id = get_user_id_from_session(session)
-    
-    if user_id == "":
-        return jsonify({"error_msg": "Bitte einloggen!"})
-    
-    return API.apt_get(user_id, apt_id)
+def user_create(first_name: str, last_name: str, email: str, passwd: str, gender: int, age: int, city: str):
+    with ORM.get_session() as session:
+        user = User()
+        
+        if user:
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.passwd = passwd
+            user.gender = gender
+            user.age = age
+            user.city = city
+            
+            session.add(user)
+            session.commit()
+            
+            return user.to_json()
+            
+    return ("{}")
 
 
-@app.route("/api/Comment/", methods=["GET"])
-@app.route("/api/Comment/<apt_id: str>", methods=["GET"])
-def comment_get(apt_id=""):
-    user_id = get_user_id_from_session(session)
-    
-    if user_id == "":
-        return jsonify({"error_msg": "Bitte einloggen!"})
-    
-    return API.comment_get(user_id, apt_id)
+def user_delete(id: str):
+    with ORM.get_session() as session:
+        user = session.scalars(select(User).where(User.id == id)).one()
+        
+        if(user):
+            session.delete(user)
+            session.commit()
+            return ("{}")
+        else:
+            return ("{}")
 
 
-if __name__ == "__main__":
-    app.run()
+def fz_get(user_id: str, fz_id: str = "") -> str:
+    with ORM.get_session() as session:
+        if fz_id != "":
+            fz = session.scalars(select(Friendzone).where(Friendzone.id == fz_id)).one()
+            
+            if fz:
+                return fz.to_json()
+            
+        elif user_id != "":
+            user = session.scalars(select(User).where(User.id == id)).one()
+            
+            if user:
+                fzs = user.friendzones
+                
+                if fzs and len(fzs) > 0:
+                    fzs_dict = {}
+                    
+                    for fz in fzs:
+                        fzs_dict[fz.id] = fz
+                
+                    return json.dumps(fzs_dict)
+            
+        return ("{}")
+
+
+def fz_create(user_id: str, name: str) -> str:
+    with ORM.get_session() as session:
+        user = session.scalars(select(User).where(User.id == id)).one()
+        
+        if user:
+            fz = Friendzone()
+            fz.creator = user.id
+            fz.name = name
+            
+            user.friendzones.append(fz)
+            
+            session.commit()
+            
+            return fz.to_json()
+            
+    return ("{}")
+
+
+def fz_update(fz_id: str, name) -> str:
+    with ORM.get_session() as session:
+        if fz_id and fz_id != "":
+            fz = session.scalars(select(Friendzone).where(Friendzone.id == fz_id)).one()
+            
+            if fz:
+                fz.name = name
+                
+                session.commit()
+                
+                return fz.to_json()
+            
+        return ("{}")
+
+
+def fz_delete(fz_id: str = "") -> str:
+    with ORM.get_session() as session:
+        fz = session.scalars(select(Friendzone).where(Friendzone.id == fz_id)).one()
+        
+        if(fz):
+            session.delete(fz)
+            session.commit()
+            return ("{}")
+        else:
+            return ("{}")
+
+
+def apt_get(user_id: str, apt_id: str = "") -> str:
+    with ORM.get_session() as session:
+        if apt_id != "":
+            apt = session.scalars(select(Appointment).where(Appointment.id == apt_id)).one()
+            
+            if apt:
+                return apt.to_json()
+            
+        elif user_id != "":
+            user = session.scalars(select(User).where(User.id == id)).one()
+            
+            if user:
+                apts = user.appointments
+                
+                if apts and len(apts) > 0:
+                    apts_dict = {}
+                    
+                    for apt in apts:
+                        apts_dict[apt.id] = apt
+                
+                    return json.dumps(apts_dict)
+            
+        return ("{}")
+    
+    
+def apt_create(user_id: str, name: str, date: datetime, time_start: str, time_stop: str, citycode: int, city: str, maxUser: int, notice: str) -> str:
+    with ORM.get_session() as session:
+        user = session.scalars(select(User).where(User.id == user_id)).one()
+        
+        if user:
+            apt = Appointment()
+            apt.creator = user.id
+            apt.name = name
+            apt.date = date
+            apt.time_start = time_start
+            apt.time_stop = time_stop
+            apt.citycode = citycode
+            apt.city = city
+            apt.maxUser = maxUser
+            apt.notice = notice
+            
+            user.appointments.append(apt)
+            
+            session.commit()
+            
+            return apt.to_json()
+            
+    return ("{}")
+
+
+def apt_update(apt_id: str, name: str, date: datetime, time_start: str, time_stop: str, citycode: int, city: str, maxUser: int, notice: str) -> str:
+    with ORM.get_session() as session:
+        if apt_id and apt_id != "":
+            apt = session.scalars(select(Appointment).where(Appointment.id == apt_id)).one()
+            
+            if apt:
+                apt.date = date
+                apt.time_start = time_start
+                apt.time_stop = time_stop
+                apt.citycode = citycode
+                apt.city = city
+                apt.maxUser = maxUser
+                apt.notice = notice
+                
+                session.commit()
+                
+                return apt.to_json()
+            
+        return ("{}")
+
+
+def apt_delete(apt_id: str = "") -> str:
+    with ORM.get_session() as session:
+        apt = session.scalars(select(Appointment).where(Appointment.id == apt_id)).one()
+        
+        if(apt):
+            session.delete(apt)
+            session.commit()
+            return ("{}")
+        else:
+            return ("{}")
+
+
+def comment_get(user_id: str = "", apt_id: str = "", comment_id: str = "") -> str:
+    with ORM.get_session() as session:
+        if comment_id != "":
+            comment = session.scalars(select(Comment).where(Comment.id == comment_id)).one()
+            
+            if comment:
+                return comment.to_json()
+            
+        elif user_id != "":
+            user = session.scalars(select(User).where(User.id == user_id)).one()
+            
+            if user:
+                comments = user.comments
+                
+                if comments and len(comments) > 0:
+                    comments_dict = {}
+                    
+                    for comment in comments:
+                        comments_dict[comment.id] = comment
+                
+                    return json.dumps(comments_dict)
+        elif apt_id != "":
+            apt = session.scalars(select(Appointment).where(Appointment.id == apt_id)).one()
+            
+            if apt:
+                comments = apt.comments
+                
+                if comments and len(comments) > 0:
+                    comments_dict = {}
+                    
+                    for comment in comments:
+                        comments_dict[comment.id] = comment
+                
+                    return json.dumps(comments_dict)
+            
+        return ("{}")
+    
+    
+def comment_create(user_id: str, apt_id: str, timestamp: str, comment_value) -> str:
+    with ORM.get_session() as session:
+        user = session.scalars(select(User).where(User.id == user_id)).one()
+        apt = session.scalars(select(Appointment).where(Appointment.id == apt_id)).one()
+        
+        if user and apt:
+            comment = Comment()
+            comment.appointment_id = apt.id
+            comment.user_id = user.id
+            comment.timestamp = timestamp
+            comment.comment_value = comment_value
+            
+            user.Comments.append(comment)
+            
+            session.commit()
+            
+            return comment.to_json()
+            
+    return ("{}")
+
+
+def comment_update(user_id: str, apt_id: str, timestamp: str, comment_value) -> str:
+    with ORM.get_session() as session:
+        user = session.scalars(select(User).where(User.id == user_id)).one()
+        apt = session.scalars(select(Appointment).where(Appointment.id == apt_id)).one()
+        
+        if user and apt:
+            comment = Comment()
+            comment.appointment_id = apt.id
+            comment.user_id = user.id
+            comment.timestamp = timestamp
+            comment.comment_value = comment_value
+            
+            user.Comments.append(comment)
+            
+            session.commit()
+            
+            return comment.to_json()
+            
+    return ("{}")
+
+
+def comment_delete(comment_id: str = "") -> str:
+    with ORM.get_session() as session:
+        comment = session.scalars(select(Comment).where(Comment.id == comment_id)).one()
+        
+        if(comment):
+            session.delete(comment)
+            session.commit()
+            return ("{}")
+        else:
+            return ("{}")
