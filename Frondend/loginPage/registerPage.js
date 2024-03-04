308d769a319aeb6dfea796c6f3aa7035d6b719d9
Felipe Costa <lipejdc@gmail.com>
2024-03-04 10:43:26 +0100

    Added Data Validation for Registration Page

--- Frondend/loginPage/registerPage.js
+++ Frondend/loginPage/registerPage.js
@@ -1,33 +1,49 @@
-var registrationForm = document.getElementById('form-registration').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
 
-    var username = document.getElementById('registration-username').value;
-    var email = document.getElementById('registration-email').value;
-    var password = document.getElementById('registration-password').value;
-    var repeatPassword = document.getElementById('repeatpassword').value;
-
-    fetch('/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username: username,
-            email: email,
-            password: password,
-            repeatPassword: repeatPassword
-        })
-    })
-    .then(response => {
-        if (response.ok) {
-            alert('Registration successful!');
-            // Redirect or perform other actions for successful login
-        } else {
-            alert('Invalid email or password.');
-            // Handle login failure
+    var registrationForm = document.getElementById('form-registration').addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        var firstName = document.getElementById('registration-firstname').value;
+        var lastName = document.getElementById('registration-lastname').value;
+        var gender = document.getElementById('registration-gender').value;
+        var age = document.getElementById('registration-age').value;
+        var city = document.getElementById('registration-city').value;
+        var registrationEmail = document.getElementById('registration-email').value;
+        var password = document.getElementById('registration-password').value;
+        var repeatPassword = document.getElementById('repeatpassword').value;
+    
+        if (password !== repeatPassword) {
+            console.log("Passwords do not match!");
+            alert('Passwords do not match');
+            event.preventDefault(); // Prevent form submission if passwords do not match
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    
+        fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstname: firstName,
+                lastname: lastName,
+                gender: gender,
+                age: age,
+                city: city,
+                email: registrationEmail,
+                password: password
+            })
+        })
+        .then(response => {
+            if (response.ok) {
+                alert('Registration successful!');
+                // Redirect or perform other actions for successful login
+            } else {
+                alert('Invalid email or password.');
+                // Handle login failure
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
 });
